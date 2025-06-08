4eace70e14415298b3c3a085ca0c89b0f4a00357
yotteiru <pelosa776@gmail.com>
2025-06-08 20:03:54 +0900

    first

---
+++ src/main/java/com/pelosa/rasutoda/RasutodaApplication.java
@@ -0,0 +1,13 @@
+package com.pelosa.rasutoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasutodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasutodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pelosa/rasutoda/RasutodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelosa.rasutoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasutodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
